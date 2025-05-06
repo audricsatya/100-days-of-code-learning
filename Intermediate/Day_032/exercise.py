eddf4ab31d20d57f1323a5963d18756b206cdf4d
audricsatya <audric.putra.satya@gmail.com>
2025-05-06 16:39:07 +0700

    modified:   Intermediate/Day_032/exercise.py

--- Intermediate/Day_032/exercise.py
+++ Intermediate/Day_032/exercise.py
@@ -1,6 +1,10 @@
 import smtplib
 
 email = "@gmail.com"
+password = "your_password"
 
 connection = smtplib.SMTP("smtp.gmail.com", 587)
 connection.starttls()
+connection.login(user=email, password=password)
+connection.sendmail(from_addr=email, to_addrs=email, 
+    msg="Subject:Hello\n\nThis is the body of the email.")
