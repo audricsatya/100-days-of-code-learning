94d09a4037ff72afce9ae276ef04b5e9dae56cf6
audricsatya <audric.putra.satya@gmail.com>
2025-06-02 16:57:36 +0700

    modified:   Intermediate/Day_032/exercise.py

--- Intermediate/Day_032/exercise.py
+++ Intermediate/Day_032/exercise.py
@@ -4,7 +4,16 @@ email = "@gmail.com"
 password = "your_password"
 
 connection = smtplib.SMTP("smtp.gmail.com", 587)
+
 connection.starttls()
+
 connection.login(user=email, password=password)
+
+designation_email = ""
+subject = "Hello"
+message = """Subject:Hello\n\nThis is the body of the email."""
+
 connection.sendmail(from_addr=email, to_addrs=email, 
-    msg="Subject:Hello\n\nThis is the body of the email.")
+    msg=message)
+
+connection.close()
