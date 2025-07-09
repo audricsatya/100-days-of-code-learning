80e03ea16356a14d684eb7f189cfa17e6b401b77
audricsatya <audric.putra.satya@gmail.com>
2025-07-09 14:39:42 +0700

    new file:   Intermediate/Day_057/.DS_Store 	new file:   Intermediate/Day_057/blog/__pycache__/post.cpython-313.pyc 	modified:   Intermediate/Day_057/blog/main.py 	modified:   Intermediate/Day_057/blog/post.py 	modified:   Intermediate/Day_057/blog/templates/index.html 	modified:   Intermediate/Day_057/blog/templates/post.html 	new file:   Intermediate/Day_057/readme.md

--- Intermediate/Day_057/blog/main.py
+++ Intermediate/Day_057/blog/main.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
+from post import Post
+import requests
+import datetime as dt
 
+CURRENT_YEAR = dt.datetime.now().year
+posts = requests.get("https://api.npoint.io/308fff4c8c069fd8cd54").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects, copy_year=CURRENT_YEAR)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post, copy_year=CURRENT_YEAR)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
---
+++ Intermediate/Day_057/readme.md
@@ -0,0 +1,12 @@
+## Day 57: Create a Blog with Templates in Python
+
+On Day 57, the focus is on building a simple blog application using Python and templates. This involves learning how to render dynamic content on web pages by separating the logic from the presentation layer. By using templates, you can efficiently manage and display blog posts, making your application more organized and maintainable.
+
+Key concepts covered:
+
+- Introduction to templating engines (such as Jinja2)
+- Structuring a basic blog application
+- Rendering dynamic content using templates
+- Passing data from Python to HTML templatess
+
+This day provides a practical foundation for developing more complex web applications in Python.
