deff812f5e3e3414f986e852e5b2df50724073d8
audricsatya <audric.putra.satya@gmail.com>
2025-07-09 14:14:10 +0700

    modified:   .DS_Store 	modified:   Intermediate/.DS_Store 	new file:   Intermediate/Day_057/blog/main.py 	new file:   Intermediate/Day_057/blog/post.py 	new file:   Intermediate/Day_057/blog/static/css/styles.css 	new file:   Intermediate/Day_057/blog/templates/index.html 	new file:   Intermediate/Day_057/blog/templates/post.html 	modified:   Intermediate/Day_057/main.py 	new file:   Intermediate/Day_057/templates/blog.html 	modified:   Intermediate/Day_057/templates/index.html

--- Intermediate/Day_057/main.py
+++ Intermediate/Day_057/main.py
@@ -5,19 +5,19 @@ import requests
 import json
 
 app = Flask(__name__)
+CURRENT_YEAR = dt.datetime.now().year
 
 @app.route('/')
 def home():
     random_number = random.randint(1, 10)
-    current_year = dt.datetime.now().year
-    return render_template('index.html', num = random_number, copy_year=current_year)
+    return render_template('index.html', num = random_number, copy_year=CURRENT_YEAR)
 
 @app.route('/guess/<name>')
 def guess(name):
     url = f"https://api.genderize.io/?name={name}"
     response = requests.get(url)
     data_gender = response.json().get('gender')
-    url = f"https://api.agify.io/?name=audric"
+    url = f"https://api.agify.io/?name={name}"
     response = requests.get(url)
     data_age = response.json().get('age')
     data = {
@@ -25,7 +25,14 @@ def guess(name):
         'gender': data_gender,
         'age': data_age
     }
-    return render_template('guess.html', data=data)
+    return render_template('guess.html', data=data, copy_year=CURRENT_YEAR)
+
+@app.route('/blog/<int:post_id>')
+def get_blog(post_id):
+    url = "https://api.npoint.io/308fff4c8c069fd8cd54"
+    response = requests.get(url)
+    blog_data = response.json()
+    return render_template('blog.html', posts=blog_data, copy_year=CURRENT_YEAR, post_id=post_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
